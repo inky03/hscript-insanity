d6e7f70cfe95859240346c0b08d5f7a398cc1fc2
ashley <84390581+inky03@users.noreply.github.com>
2025-12-28 15:20:49 -0600

    hashlink macro stuf
    well it only affects Math but it should be applicacble to most std classes eventualy

--- insanity/Config.hx
+++ insanity/Config.hx
@@ -1,9 +1,14 @@
 package insanity;
 
 import insanity.custom.*;
+#if hl import insanity.custom.HL; #end
 
 class Config {
 	@:unreflective public static var typeProxy:Map<String, Dynamic> = [
+		#if hl
+		'Math' => HLMath,
+		#end
+		
 		'Reflect' => InsanityReflect,
 		'Type' => InsanityType,
 		'Std' => InsanityStd
