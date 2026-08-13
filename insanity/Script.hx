c6f115e83243bb84b49db83f6199becabd5e9b35
ashley <84390581+inky03@users.noreply.github.com>
2026-08-13 16:31:47 -0600

    script doc zzz

--- insanity/Script.hx
+++ insanity/Script.hx
@@ -7,17 +7,42 @@ import insanity.backend.Expr;
 
 @:access(insanity.backend.Interp)
 class Script {
+	/**
+	 * Origin / name of the script (this is used for error reporting).
+	 */
 	public var name:String;
 	
+	/**
+	 * The script's parser.
+	 */
 	public var parser:Parser = new Parser();
+	/**
+	 * The script's interpreter.
+	 */
 	public var interp:Interp = null;
+	/**
+	 * The script's program (expression).
+	 */
 	public var program:Expr = null;
 	
+	/**
+	 * Whether the script failed when trying to start.
+	 */
 	public var failed:Bool = false;
 	
+	/**
+	 * The script's global variables.
+	 */
 	public var variables(get, never):Map<String, Dynamic>;
 	inline function get_variables():Map<String, Dynamic> { return interp.variables; }
 	
+	/**
+	 * Creates a `Script` and parses a code string.
+	 * 
+	 * @param	string		The code string to parse.
+	 * @param	name		The origin/name of the script (this is used for error reporting).
+	 * @param	environment	The `Environment` to run this script in.
+	 */
 	public function new(string:String, name:String = 'hscript', ?environment:Environment):Void {
 		parser.allowTypes = parser.allowJSON = parser.allowMetadata = true;
 		interp = Type.createInstance(Config.interpClass, [environment, this]);
@@ -28,6 +53,13 @@ class Script {
 		parse(string);
 	}
 	
+	/**
+	 * Parses code from a string.
+	 * If the code fails to parse, `onParsingError` will be called, otherwise, the expression will be stored in `program`.
+	 * 
+	 * @param	string		The code string to parse.
+	 * @return	The script expression, or null if the code failed to parse.
+	 */
 	public function parse(string:String):Expr {
 		try {
 			program = parser.parseScript(string, name);
@@ -39,7 +71,13 @@ class Script {
 		return program;
 	}
 	
-	public function start(?expr:Expr):Any {
+	/**
+	 * Starts the script program.
+	 * If the program fails to start, `onProgramError` will be called.
+	 * 
+	 * @return	The return value of the program, if any.
+	 */
+	public function start():Any {
 		try {
 			if (program == null) throw 'Program is uninitialized';
 			
@@ -60,6 +98,13 @@ class Script {
 		return null;
 	}
 	
+	/**
+	 * Calls a function on the script.
+	 * 
+	 * @param	variable	The name of the function.
+	 * @param	args		Arguments to pass when calling the function.
+	 * @return	The return value of the function, if any.
+	 */
 	public function call(variable:String, ?args:Array<Dynamic>):Any {
 		if (interp == null) throw 'Interpreter is uninitialized';
 		
@@ -73,6 +118,10 @@ class Script {
 		return Reflect.callMethod(interp, fun, args ?? []);
 	}
 	
+	/**
+	 * Initializes default variables within the script.
+	 * This can be overridden to define new variables.
+	 */
 	public function setDefaults():Void {
 		interp.setDefaults();
 		
@@ -81,10 +130,22 @@ class Script {
 		variables.set('interp', interp);
 	}
 	
-	public dynamic function onParsingError(e:haxe.Exception):Void {
-		trace('Failed to initialize script program!\n' + e.details());
+	/**
+	 * This function is called when the script encounters an error when parsing.
+	 * Can be overridden to execute custom behavior.
+	 * 
+	 * @param	exception	The exception that caused the parsing to halt.
+	 */
+	public dynamic function onParsingError(exception:haxe.Exception):Void {
+		trace('Failed to initialize script program!\n' + exception.details());
 	}
-	public dynamic function onProgramError(e:haxe.Exception):Void {
-		trace('Script program stopped unexpectedly!\n' + e.details());
+	/**
+	 * This function is called when the script program encounters an error when first running.
+	 * Can be overridden to execute custom behavior.
+	 * 
+	 * @param	exception	The exception that caused the parsing to halt.
+	 */
+	public dynamic function onProgramError(exception:haxe.Exception):Void {
+		trace('Script program stopped unexpectedly!\n' + exception.details());
 	}
 }
